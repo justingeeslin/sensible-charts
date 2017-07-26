3f7b5f5d84934b0e5bbde1fc4b48b16b52cfb01c
Justin Geeslin <justin@geesl.in>
2017-07-26 14:34:05 -0500

    PieChart upgraded to work with d3 v4

--- js/SensiblePieChart.js
+++ js/SensiblePieChart.js
@@ -1,107 +1,52 @@
-const $ = require('jquery');
-window.$ = $;
-const c3 = require('c3');
+const extend = require('extend');
+const d3 = require('d3');
+const d3pie = require('d3pie');
 
 function SensiblePieChart( options ) {
   var self = this;
 
   var defaults = {
-    target: $(document.body),
-    data: ['Visitors', 100],
+    sel: '#pie',
+    data: {
+  		content: [
+  			{ label: "Chickens", value: 5 },
+  			{ label: "Elephants", value: 8 },
+  			{ label: "Java", value: 2 }
+  		]
+  	},
+    // data: ['Visitors', 100],
     // data: {
     //   'root': [['Visitors', 100]],
     //   'Visitors': [['Mobile', 75],['Desktop', 24]],
     //   'Desktop': [['Windows', 75],['Mac', 24], ['Other', 1]]
     // },
-    colors: ['#FF0000', '#00FF00', '#0000FF']
+    colors: ['#333', '#444', '#555'],
+    effects: {
+  		load: {
+  			effect: "none"
+  		}
+  	}
   }
 
-  var drillDownPath = ['root'];
+  self = extend(this, defaults)
+  self = extend(this, options)
 
-  var currentLayer = 'root';
+  //Augment data with colors
+  var colorI = 0
+  for( var i in this.data.content) {
 
-  $.extend(self, defaults, options)
+    this.data.content[i].color = this.colors[colorI];
 
-  this.chart = c3.generate({
-    bindto: self.target[0],
-    data: {
-      columns: [],
-      type : 'pie',
-      colors: self.colors,
-      onclick: function (d, i) {
-        if (typeof self.data === 'object' && self.data.hasOwnProperty(d.id)) {
-          console.log('Attempting to drill down to: ' + d.id);
-          self.drilldown(d.id)
-        }
-      },
-      onmouseover: function (d, i) {  },
-      onmouseout: function (d, i) {  }
+    colorI++;
+
+    if (colorI >= this.colors.length) {
+      colorI = 0;
     }
-  });
-
-  this.drilldown = function(label) {
-    if (typeof self.data[label] === 'undefined') {
-      console.log('No layer under this one.')
-      return;
-    }
-
-    console.log('Drilling down to ' + label + ' layer.')
-    console.log('Columns are now: ', self.data[label])
-
-    drillDownPath.push(label)
-
-    var dataToUnload = []
-    for (var i in self.data[currentLayer]) {
-      dataToUnload.push(self.data[currentLayer][i][0])
-    }
-    console.log('Unloading..', dataToUnload)
-
-    this.chart.load({
-      unload: dataToUnload,
-      columns: this.data[label]
-    });
-
-    //Create a color object for C3
-    var newColors = {}
-    // Just cycle through the colors.
-    var currentColor = -1;
-    function nextColor() {
-      // if you go beyond the index...
-      if (self.colors[currentColor] === undefined ) {
-        // ...reset it to zero
-        currentColor = 0;
-      }
-      return self.colors[currentColor++];
-    }
-    // Get the keys of the data.
-    for( var i in this.data[label]) {
-      newColors[this.data[label][i][0]] = nextColor();
-    }
-
-    console.log('Setting colors like so:', newColors)
-    this.chart.data.colors(newColors)
-
-    currentLayer = label;
   }
 
-  this.up = function() {
-    console.log('Going up.')
-    self.drilldown(drillDownPath[drillDownPath.length-2]);
-  }
 
-  this.target.on('contextmenu', this.up);
 
-  // Load the data if the data is an array
-  if (Array.isArray(this.data)) {
-    this.chart.load({
-      columns: [
-        this.data
-      ]
-    });
-  }
-  else if (typeof this.data === 'object') {
-    this.drilldown('root');
-  }
+  var pie = new d3pie(d3.select(this.sel).node(), this);
 
   return this;
 }
--- tests/SensiblePieChart.js
+++ tests/SensiblePieChart.js
@@ -1,20 +1,32 @@
 describe('SensiblePieChart', function() {
 
 		beforeAll(function() {
-			$(document.body).append($('<div class="chart visitors"></div>'));
+
 		});
 
 		it('should construct', function() {
-			myFirstPieChart = new SensiblePieChart({
-				target: $('.chart.visitors'),
-			  data: {
-			    'root': [['Visitors', 100]],
-			    'Visitors': [['Mobile', 75],['Desktop', 24]],
-			    'Desktop': [['Windows', 75],['Mac', 24], ['Other', 1]]
-			  },
-			});
+			$(document.body).append($('<div class="chart visitors" id="pie"></div>'));
+			myFirstPieChart = new SensiblePieChart();
 			expect(myFirstPieChart instanceof SensiblePieChart).toBe(true)
-			expect($('.chart.visitors').children('svg').length > 0).toBe(true)
+			expect($('#pie').children('svg').length > 0).toBe(true)
+    });
+
+		it('should display custom data', function() {
+			$(document.body).append($('<div class="chart visitors" id="pie2"></div>'));
+			myFirstPieChart = new SensiblePieChart({
+				sel: '#pie2',
+			  data: {
+		  		content: [
+		  			{ label: "Snoopy", value: 5 },
+		  			{ label: "Charlie Brown", value: 8 },
+		  			{ label: "Peppermint Patty", value: 2 }
+		  		]
+		  	},
+			});
+			window.setTimeout(function() {
+				expect(d3.select('#pie2').select('svg').text()).toBe("33%53%13%SnoopyCharlie BrownPeppermint Patty")
+			}, 50)
+
     });
 
 		afterAll(function() {
