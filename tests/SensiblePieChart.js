130976e3b33e56d5320e4adec2005b1fa7cb8b2a
Justin Geeslin <justin@geesl.in>
2017-07-26 21:22:40 -0500

    Testing colors where there are less colors than their are slices of pie

--- tests/SensiblePieChart.js
+++ tests/SensiblePieChart.js
@@ -30,6 +30,26 @@ describe('SensiblePieChart', function() {
 
     });
 
+		it('should test color cycle', function(done) {
+			$(document.body).append($('<div class="chart visitors" id="pie3"></div>'));
+			myFirstPieChart = new SensiblePieChart({
+				sel: '#pie3',
+			  data: {
+		  		content: [
+		  			{ label: "Snoopy", value: 5 },
+		  			{ label: "Charlie Brown", value: 8 },
+		  			{ label: "Peppermint Patty", value: 2 }
+		  		]
+		  	},
+				colors: ['#0f0', '#00f']
+			});
+			window.setTimeout(function() {
+				expect(d3.select('#pie3').select('svg').text()).toBe("33%53%13%SnoopyCharlie BrownPeppermint Patty")
+				done()
+			}, 50)
+
+    });
+
 		afterAll(function() {
 
 		});
